45e022d9058151d3ba2603901474b16652257d10
Martin Plascencia <me@martyroque.com>
2019-04-03 21:01:47 -0400

    Updated the site metadata and added react helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,15 @@
 module.exports = {
   siteMetadata: {
-    name: `Hello Typescript World!`,
-    tagline: `Gatsby + SASS + Typescript = 💪`
+    name: `Hello! My name is Luis Roque`,
+    tagline: `I'm a Full Stack Developer based in Michigan`,
+    title: `Luis Roque's Website`,
+    siteUrl: `https://roque.dev`,
+    description: `I'm a Full Stack Developer based in Michigan`,
   },  
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { graphql } from 'gatsby';
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import * as styles from './Index.module.scss';
 
 interface IndexPageProps {
@@ -8,6 +9,7 @@ interface IndexPageProps {
       siteMetadata: {
         name: string;
         tagline: string;
+        title: string;
       },
     },
   };
@@ -19,21 +21,26 @@ export const indexPageQuery = graphql`
       siteMetadata {
         name
         tagline
+        title
       }
     }
   }
 `;
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
-
   public render() {
     const {
       name,
       tagline,
+      title,
     } = this.props.data.site.siteMetadata;
 
     return (
       <div className={styles.Container}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{title}</title>
+        </Helmet>
         <h1>{name}</h1>
         <p>{tagline}</p>
       </div>
